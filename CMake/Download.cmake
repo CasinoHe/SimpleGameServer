7f767709d0560d5e06a6828d192dd6ca9a4c8c3b
CasinoHe <CasinoHe@outlook.com>
2018-10-29 17:12:13 +0800

    fix check sha256 bug

--- CMake/Download.cmake
+++ CMake/Download.cmake
@@ -6,11 +6,11 @@ function(download_and_unzip_package package_name package_url package_tar_name pa
 	if (EXISTS ${package_tar_name})
 		set(FILE_SHA256 "")
 		file(SHA256 ${package_tar_name} FILE_SHA256)
-		if (FILE_SHA256 NOT STREQUAL package_sha256)
-			message(STATUS "SHA256 checking failed!")
+		if (NOT FILE_SHA256 STREQUAL package_sha256)
+			message(STATUS "SHA256 checking ${package_name} failed!")
 		else ()
 			set(DOWNLOAD_FILE FALSE)
-			message(STATUS "Found download file")
+			message(STATUS "Found ${package_name} download file")
 		endif ()
 	endif ()
 
@@ -24,13 +24,13 @@ function(download_and_unzip_package package_name package_url package_tar_name pa
 	if (NOT EXISTS ${package_file_name})
 		message(STATUS "unzip file: ${package_file_name}")
 		file(MAKE_DIRECTORY ${package_file_name})
-		execute_process(COMMAND CMAKE -E tar -xzf ${package_tar_name} -C ${PACKAGE_DIR}
+		execute_process(COMMAND tar -xzf ${package_tar_name} -C ${PACKAGE_DIR}
 			WORKING_DIRECTORY ${SOURCE_ROOT_DIR}
 			RESULT_VARIABLE TAR_RESULT
 			OUTPUT_VARIABLE TAR_OUTPUT
 			ERROR_VARIABLE TAR_ERROR)
 		
-		if (${TAR_ERROR} NOT STREQUAL "")
+		if (NOT ${TAR_ERROR} STREQUAL "")
 			message(FATAL_ERROR "Decompress ${package_name} FAILED.Message: ${TAR_ERROR}")
 		endif ()
 	else ()
