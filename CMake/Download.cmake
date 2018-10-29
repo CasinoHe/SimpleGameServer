1507ddd8dff554063fbff336ce984a700c0901dd
CasinoHe <CasinoHe@outlook.com>
2018-10-29 17:01:10 +0800

    add cmake for download boost

---
+++ CMake/Download.cmake
@@ -0,0 +1,39 @@
+# cmake download functions
+
+function(download_and_unzip_package package_name package_url package_tar_name package_file_name package_sha256)
+	message(STATUS "Checking ${package_name} files .....")
+	set(DOWNLOAD_FILE TRUE)
+	if (EXISTS ${package_tar_name})
+		set(FILE_SHA256 "")
+		file(SHA256 ${package_tar_name} FILE_SHA256)
+		if (FILE_SHA256 NOT STREQUAL package_sha256)
+			message(STATUS "SHA256 checking failed!")
+		else ()
+			set(DOWNLOAD_FILE FALSE)
+			message(STATUS "Found download file")
+		endif ()
+	endif ()
+
+	if (DOWNLOAD_FILE)
+		message(STATUS "downloading file: ${package_url}")
+		file(DOWNLOAD ${package_url} ${package_tar_name}
+			SHOW_PROGRESS
+			EXPECTED_HASH SHA256=${package_sha256})
+	endif ()
+
+	if (NOT EXISTS ${package_file_name})
+		message(STATUS "unzip file: ${package_file_name}")
+		file(MAKE_DIRECTORY ${package_file_name})
+		execute_process(COMMAND CMAKE -E tar -xzf ${package_tar_name} -C ${PACKAGE_DIR}
+			WORKING_DIRECTORY ${SOURCE_ROOT_DIR}
+			RESULT_VARIABLE TAR_RESULT
+			OUTPUT_VARIABLE TAR_OUTPUT
+			ERROR_VARIABLE TAR_ERROR)
+		
+		if (${TAR_ERROR} NOT STREQUAL "")
+			message(FATAL_ERROR "Decompress ${package_name} FAILED.Message: ${TAR_ERROR}")
+		endif ()
+	else ()
+		message(STATUS "Decompress file exists.Skip.")
+	endif ()
+endfunction ()
---
+++ CMake/ThirdParty.cmake
@@ -0,0 +1,15 @@
+# Download(git clone) and compile all third party repository
+
+# configure for boost
+set(BOOST_MAJOR_VERSION 1)
+set(BOOST_MINOR_VERSION 68)
+set(BOOST_PATCH_VERSION 0)
+set(BOOST_SHA256 "da3411ea45622579d419bfda66f45cd0f8c32a181d84adfa936f5688388995cf")
+set(BOOST_VERSION "boost_${BOOST_MAJOR_VERSION}_${BOOST_MINOR_VERSION}_${BOOST_PATCH_VERSION}")
+set(BOOST_DOWNLOAD_URL "https://dl.bintray.com/boostorg/release/${BOOST_MAJOR_VERSION}.${BOOST_MINOR_VERSION}.${BOOST_PATCH_VERSION}/source/${BOOST_VERSION}.tar.gz")
+set(BOOST_TAR_FILENAME ${PACKAGE_DIR}/${BOOST_VERSION}.tar.gz)
+set(BOOST_UNZIP_FILENAME ${PACKAGE_DIR}/${BOOST_VERSION})
+
+include(Download)
+
+download_and_unzip_package(boost ${BOOST_DOWNLOAD_URL} ${BOOST_TAR_FILENAME} ${BOOST_UNZIP_FILENAME} ${BOOST_SHA256})
