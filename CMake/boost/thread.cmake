6a3b43ca22efa3e750889c76f755580f9152aa12
CasinoHe <CasinoHe@outlook.com>
2018-11-02 16:39:18 +0800

    modify boost compile flags

--- CMake/boost/thread.cmake
+++ CMake/boost/thread.cmake
@@ -1,10 +1,19 @@
-set(SRC_FILES
-	${BOOST_ROOT_DIR}/libs/thread/src/pthread/thread.cpp
-	# ${BOOST_ROOT_DIR}/libs/thread/src/pthread/once.cpp
-	${BOOST_ROOT_DIR}/libs/thread/src/pthread/once_atomic.cpp
-	${BOOST_ROOT_DIR}/libs/thread/src/future.cpp
-)
+if (WIN32)
+	set(SRC_FILES
+		${BOOST_ROOT_DIR}/libs/thread/src/win32/thread.cpp
+		${BOOST_ROOT_DIR}/libs/thread/src/win32/tss_pe.cpp
+		${BOOST_ROOT_DIR}/libs/thread/src/future.cpp
+		)
+else ()
+	set(SRC_FILES
+		${BOOST_ROOT_DIR}/libs/thread/src/pthread/thread.cpp
+		# ${BOOST_ROOT_DIR}/libs/thread/src/pthread/once.cpp
+		${BOOST_ROOT_DIR}/libs/thread/src/pthread/once_atomic.cpp
+		${BOOST_ROOT_DIR}/libs/thread/src/future.cpp
+		)
+endif ()
 
 add_library(boost_thread OBJECT ${SRC_FILES})
 target_compile_options(boost_thread PRIVATE -DBOOST_THREAD_STATIC_LINK=1 -DBOOST_THREAD_POSIX -DBOOST_THREAD_BUILD_LIB=1)
+target_compile_definitions(boost_thread PRIVATE BOOST_THREAD_POSIX)
 set_property(TARGET boost_thread PROPERTY CXX_STANDARD 11)
--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+// Purpose: Server entry
+// Created: 2018.11.1
+// By: CasinoHe
 
-int main() {
-	std::cout << "test" << std::endl;
+#include <iostream>
+#include <boost/chrono.hpp>
+#include <boost/thread.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+int main(int argc, char **argv) {
+	std::cout << "Simple server stoped" << std::endl;
 	return 0;
 }
