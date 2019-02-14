2e1a8672d36e631b81a234a2ad4db10c693e0549
Casino <CasinoHe@outlook.com>
2019-02-14 04:01:59 +0000

    add player directory

---
+++ player/player_object.cpp
@@ -0,0 +1,9 @@
+// Purpose: player object for onlien player
+// Date: 2018.12.21
+// By: CasinoHe
+
+#include "player/player_object.h"
+
+
+namespace simple_server {
+}
---
+++ player/player_object.h
@@ -0,0 +1,26 @@
+// Purpose: player object for onlien player
+// Date: 2018.12.21
+// By: CasinoHe
+
+#pragma once
+
+#ifndef SIMPLE_SERVER_PLAYER_PLAYER_OBJECT_H
+#define SIMPLE_SERVER_PLAYER_PLAYER_OBJECT_H
+
+#include "object/game_object.h"
+
+namespace simple_server {
+	class CPlayerObject: public CGameObject {
+
+		// Player Object need to be serializable
+		SERIALIZE_CLASS_HEAD;
+
+		public:
+			CPlayerObject();
+			virtual ~CPlayerObject();
+
+			inline virtual bool is_serializable() {return true;}
+	};
+}
+
+#endif
